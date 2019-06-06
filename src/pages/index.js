33c632c81d790352522686d248eb5fdd9969cef9
Jorge <jorgecbll@gmail.com>
2019-06-06 17:45:30 -0300

    Fixed Shop path

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import Shop from "../components/shop"
+import Shop from "../components/Shop"
 import Context from "../context/Context"
 const IndexPage = () => {
   return (
